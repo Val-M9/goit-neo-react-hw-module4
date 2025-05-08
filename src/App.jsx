f78530474c6db4735da867522a81e273ac6b19d7
Valera <v.mischenko90@gmail.com>
2025-05-08 13:30:10 +0300

    homework done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,92 @@
-import { useState, useEffect } from 'react'
-import { Container, ContactForm, ContactList, SearchBox } from './components'
+import { useState } from 'react'
+import {
+  Container,
+  ImageGallery,
+  SearchBar,
+  ImageModal,
+  LoadMoreBtn,
+  Loader,
+  ErrorMessage,
+} from './components'
+import imagesService from './services/images-service'
 
 function App() {
-  const [records, setRecords] = useState(
-    JSON.parse(localStorage.getItem('contacts')) || []
-  )
-  const [searchValue, setSearchValue] = useState('')
+  const [images, setImages] = useState([])
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
+  const [query, setQuery] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(null)
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(records))
-  }, [records])
+  const handleSearch = async (newQuery) => {
+    setError(null)
+    setQuery(newQuery)
+    setPage(1)
+    setImages([])
+    handleLoadImages(newQuery, 1)
+  }
 
-  const filteredRecords = records.filter((record) =>
-    record.name.toLowerCase().includes(searchValue.toLowerCase())
-  )
+  const handleLoadMore = () => {
+    const nextPage = page + 1
+    setPage(nextPage)
+    handleLoadImages(query, nextPage)
+  }
+
+  const handleLoadImages = async (searchQuery, currentPage) => {
+    try {
+      setIsLoading(true)
+      const data = await imagesService.searchImages(searchQuery, currentPage)
+      if (data.results.length === 0) {
+        setError('Sorry, no images found. Please try a different search query.')
+      } else {
+        setImages((prevImages) =>
+          currentPage === 1 ? data.results : [...prevImages, ...data.results]
+        )
+        setTotalPages(data.total_pages)
+      }
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const handleOpenModal = (id) => {
+    const image = images.find((image) => image.id === id)
+    setSelectedImage(image)
+    setIsModalOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+    setSelectedImage(null)
+  }
 
   return (
-    <Container>
-      <h1>Phonebook</h1>
-      <ContactForm onAddRecord={setRecords} />
-      <SearchBox onSearch={setSearchValue} searchValue={searchValue} />
-      <ContactList records={filteredRecords} onDeleteRecord={setRecords} />
-    </Container>
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      <Container>
+        {!error ? (
+          <ImageGallery images={images} onOpenModal={handleOpenModal} />
+        ) : (
+          <ErrorMessage message={error} />
+        )}
+        <Loader isLoading={isLoading} />
+      </Container>
+      {images.length > 0 && page < totalPages && (
+        <LoadMoreBtn onLoadMore={handleLoadMore} />
+      )}
+      {selectedImage && (
+        <ImageModal
+          isOpen={isModalOpen}
+          imageRegular={selectedImage?.urls?.regular}
+          description={selectedImage?.description}
+          onClose={handleCloseModal}
+        />
+      )}
+    </>
   )
 }
 
